7ce4c62491f6acd593e9c18190a7555852a8ad03
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2021-04-26 13:06:47 +0200

    Added function for centering the source pictures for minion tile
    M3

--- src/components/game/Minion.js
+++ src/components/game/Minion.js
@@ -15,10 +15,25 @@ class Minion extends Component {
 
         // load the picture
         var minion = new Image();
-        minion.onload = function() {
-            ctx.drawImage(minion, 1, 1);
-        };
-        minion.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png';
+        minion.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png';
+        
+        // scale picture down an put into canvas
+        minion.onload = function () {
+
+            // calculate width and height of the scaled image
+            var wrh = minion.width / minion.height; // ratio
+            var newWidth = canvas.width;
+            var newHeight = newWidth / wrh;
+            if (newHeight > canvas.height) {
+                newHeight = canvas.height;
+                newWidth = newHeight * wrh;
+              }
+            // calculate offset
+            var xOffset = newWidth < canvas.width ? ((canvas.width - newWidth) / 2) : 0;
+            var yOffset = newHeight < canvas.height ? ((canvas.height - newHeight) / 2) : 0;
+              // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+              ctx.drawImage(minion, xOffset, yOffset, newWidth, newHeight);
+          };
 
         //ctx.fillRect(0, 0, this.state.canvasWidth, this.state.canvasHeight);
     }
